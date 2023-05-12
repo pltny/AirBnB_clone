f79c421b16ab26227619f1b73ebe44859723a481
Derick Gitonga <gitongaderick17@gmail.com>
2023-05-12 15:27:39 -0700

    updated

---
+++ one/models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creates class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """object"""
+
+    state_id = ""
+    name = ""
---
+++ one/models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
